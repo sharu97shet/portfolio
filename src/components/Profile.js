f3e38bde0130a4af2842aa52b6d51d44ccc0ac60
sharu97shet <sharathshet.n@gmail.com>
2023-11-04 16:48:03 +0530

    home page with social icons

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import '../App.css';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 const Profile = () => {
   return (
     <div className='bioprofile'>
-       <div>
-        <h1>profile</h1>
-        <h3> Sharath shet</h3>
-        <h3>Full Stack Developer</h3>
+       <div className='wrapper'>
+        <h1 className='display-4'>Hi , I am   Sharath Shet</h1>
+        
+        <p className='headline'>I am Software Developer ,
+        Specialied in Backend Development with Full-Stack Development Skills Focused on Backend Mastery</p>
+
+        <div>
+        <a href="https://github.com/sharu97shet" class="fa fa-github" target="_blank" rel="noopener noreferrer" ></a>
+        <a href="https://www.linkedin.com/in/sharathshet" class="fa fa-linkedin" target="_blank" rel="noopener noreferrer" ></a>
+
+        <a href="#" class="fa fa-youtube"  rel="noopener noreferrer" ></a>
+        <a href="#" class="fa fa-instagram" rel="noopener noreferrer" ></a>
+            
+    </div>
+       
         </div>
     </div>
   )
