878b2e12c86858dc67748dc89744f376ecd456cd
Sharath N Shet <111754387+sharu97shet@users.noreply.github.com>
2023-11-04 07:02:35 +0530

    first commit

---
+++ src/components/Profile.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../App.css';
+
+const Profile = () => {
+  return (
+    <div className='bioprofile'>
+       <div>
+        <h1>profile</h1>
+        <h3> Sharath shet</h3>
+        <h3>Full Stack Developer</h3>
+        </div>
+    </div>
+  )
+}
+
+export default Profile
