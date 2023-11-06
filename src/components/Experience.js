9280534a7edb824f9246cf89e6c654f72bb3c44a
Sharath N Shet <111754387+sharu97shet@users.noreply.github.com>
2023-11-06 23:07:07 +0530

    development branch experience with css code

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Experience = () => {
   return (
-    <div>Experience</div>
+    <div className='experience'>
+     <h1>Experience</h1> 
+     <h3>Access Automation Software Pvt LTD</h3>
+     <p>Backend Web Developer</p>
+
+     <h3>Warp Drive Tech Works</h3>
+     <p>Junior  Web Developer</p>
+
+
+  
+  
+  </div>
   )
 }
 
