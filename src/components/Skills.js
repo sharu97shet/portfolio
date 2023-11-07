2627d1b46751f5b2cbdbce6886b664d46beef325
Sharath N Shet <111754387+sharu97shet@users.noreply.github.com>
2023-11-07 23:15:24 +0530

    push skills and app.css file

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,50 +4,91 @@ import '../App.css';
 const Skills = () => {
   return (
     
-    <div className='skill'>
+    <div className='headingskill'>
 
-      <section>
+<section id="skills" class="skills section-bg">
+
+<div class="container" data-aos="fade-up">
 
       <h2>Skills</h2>
       
       <div className='container mt-4'>
 
-     <div className='row'>
+      <div class="row skills-content">
 
       <div className='col-lg 6'>
         
 
-     <h4 class="course">HTML</h4>   
+     {/* <h4 class="course">HTML</h4>    */}
      <div class="progress">
-        
+     <span class="skill">HTML <i class="val">90%</i></span>  
      
+     <div class="progress-bar-wrap">
     <div class="progress-bar" role="progressbar" aria-valuenow="50"  style={{width: "90%"}} aria-valuemin="0" 
     aria-valuemax="100"></div>
+    </div>
 
-   <span>90%</span> 
-   
-     
-    
-    </div>  
+     </div>  
 
-    <br/>
+  <div class="progress">
 
-    <h4 class="course">CSS</h4> 
-    <div class="progress">
+    <span class="skill">CSS <i class="val">75%</i></span>  
 
+    <div class="progress-bar-wrap">
      <div class="progress-bar" role="progressbar" aria-valuenow="50"  style={{width: "75%"}} aria-valuemin="0" 
     aria-valuemax="100"></div>
+    </div>
 
-<span>75%</span> 
+    </div>  
 
-       </div>  
+    <div class="progress">
 
-    
+    <span class="skill">Javascript <i class="val">80%</i></span>  
 
-   </div>
+    <div class="progress-bar-wrap">
+     <div class="progress-bar" role="progressbar" aria-valuenow="50"  style={{width: "80%"}} aria-valuemin="0" 
+    aria-valuemax="100"></div>
+    </div>
+
+    </div>  
+
+    </div>
 
        
  <div className='col-md 6'>
+
+ <div class="progress">
+     <span class="skill">MySQL <i class="val">90%</i></span>  
+     
+     <div class="progress-bar-wrap">
+    <div class="progress-bar" role="progressbar" aria-valuenow="50"  style={{width: "90%"}} aria-valuemin="0" 
+    aria-valuemax="100"></div>
+    </div>
+
+     </div>  
+
+  <div class="progress">
+
+    <span class="skill">🐍python  <i class="val">75%</i></span>  
+
+    <div class="progress-bar-wrap">
+     <div class="progress-bar" role="progressbar" aria-valuenow="50"  style={{width: "75%"}} aria-valuemin="0" 
+    aria-valuemax="100"></div>
+    </div>
+
+    </div>  
+
+    <div class="progress">
+
+    <span class="skill">MONGODB <i class="val">80%</i></span>  
+
+    <div class="progress-bar-wrap">
+     <div class="progress-bar" role="progressbar" aria-valuenow="50"  style={{width: "80%"}} aria-valuemin="0" 
+    aria-valuemax="100"></div>
+    </div>
+
+    </div>  
+
 
  
        
@@ -62,6 +103,8 @@ const Skills = () => {
       </div>
 
 
+
+</div>
 
       </section>
 
