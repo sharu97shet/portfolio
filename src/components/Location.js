5ddc354a27d4a86ccea9892c2127873dcf566cad
sharu97shet <sharathshet.n@gmail.com>
2023-11-16 17:53:21 +0530

    commit loc

---
+++ src/components/Location.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Location = () => {
+  return (
+    <div>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.0593292645644!2d77.52588477354678!3d12.968055414959052!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae3d9c6c2adeb5%3A0xb56615da52ea68fd!2sGKW%20Layout%20Bus%20Stand!5e0!3m2!1sen!2sin!4v1700136969312!5m2!1sen!2sin" width={1350} height={400} style={{border:"0"}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+  )
+}
+
+export default Location
