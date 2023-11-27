9af7c1fe778fd883f021e0cda17fe3fadc428130
sharu97shet <sharathshet.n@gmail.com>
2023-11-27 17:09:15 +0530

    commit experience

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+
+ import  './footer.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/bootstrap-icons.css'
 // import '@fortawesome/fontawesome-free/css/all.min.css';
@@ -9,6 +11,7 @@ import Skills from './components/Skills';
 import About from './components/About';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import Location from './components/Location';
 function App() {
   return (
     <div className="App">
@@ -22,6 +25,7 @@ function App() {
      <hr></hr>
      <Contact/>
      <hr></hr>
+     <Location/>
      <Footer/>
      
     </div>
